3ec5c382fb215f6a620424fd785eb050c5996da7
BLOUDAN JEWELLERY <161647023+BLOUDANJEWELLERY@users.noreply.github.com>
2025-07-28 09:45:51 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -43,11 +43,3 @@ async function loadBanner() {
 }
 
 document.addEventListener("DOMContentLoaded", loadBanner);
-
-function formatTo12Hour(time24) {
-  const [hourStr, minute] = time24.split(':');
-  let hour = parseInt(hourStr, 10);
-  const ampm = hour >= 12 ? 'PM' : 'AM';
-  hour = hour % 12 || 12; // convert 0 to 12
-  return `${hour}:${minute} ${ampm}`;
-}
